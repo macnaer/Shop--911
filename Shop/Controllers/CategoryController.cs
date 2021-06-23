5431dfd0b345a635b3e80893affebb6139de738e
trofimchuk <trofimchuk@itstep.academy>
2021-06-23 11:38:22 +0300

    Start witch add category

--- Shop/Controllers/CategoryController.cs
+++ Shop/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data;
+using Shop.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,29 @@ namespace Shop.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
+        {
+            IEnumerable<Category> categoryList = _db.Category;
+            return View(categoryList);
+        }
+
+        // GET
+        public IActionResult Create()
         {
             return View();
         }
+
+        // POST
+        //public IActionResult Create(obj)
+        //{
+        //    return View();
+        //}
     }
 }
