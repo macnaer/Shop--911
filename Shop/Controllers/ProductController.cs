2e87b6c757cef0566e2fd603aa98298501163179
trofimchuk <trofimchuk@itstep.academy>
2021-06-28 11:46:49 +0300

    Delete product

--- Shop/Controllers/ProductController.cs
+++ Shop/Controllers/ProductController.cs
@@ -121,5 +121,50 @@ namespace Shop.Controllers
 
             return View(productVM);
         }
+
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product product = _db.Product.Include(u => u.Category).FirstOrDefault(u => u.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+
+
+        //POST - DELETE
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Product.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            string upload = _webHostEnvironment.WebRootPath + ENV.ImagePath;
+            var oldFile = Path.Combine(upload, obj.Image);
+
+            if (System.IO.File.Exists(oldFile))
+            {
+                System.IO.File.Delete(oldFile);
+            }
+            _db.Product.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+
+
     }
 }
