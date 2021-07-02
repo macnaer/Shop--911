a40bfe3986895cf9ba5ccc7f92154588dadf8c65
trofimchuk <trofimchuk@itstep.academy>
2021-07-02 11:18:49 +0300

    Shopping cart

--- Shop/Controllers/HomeController.cs
+++ Shop/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Shop.Data;
 using Shop.Models;
 using Shop.Models.ViewModels;
+using Shop.Services;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -37,12 +39,43 @@ namespace Shop.Controllers
 
         public IActionResult Details(int id)
         {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+
+            if(HttpContext.Session.Get<IEnumerable<ShoppingCart>>(ENV.SessinCart) != null && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(ENV.SessinCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(ENV.SessinCart);
+            }
+
             DetailsVM detailsVM = new DetailsVM()
             {
-                Product = _db.Product.Include(u => u.Category).Where(u => u.Id == id).FirstOrDefault()
+                Product = _db.Product.Include(u => u.Category).Where(u => u.Id == id).FirstOrDefault(), InCart = false
             };
 
+            foreach (var item in shoppingCartList)
+            {
+                if (item.ProductId == id)
+                {
+                    detailsVM.InCart = true;
+                }
+            }
+
             return View(detailsVM);
+        }
+
+        [HttpPost, ActionName("Details")]
+        public IActionResult DetailsPost(int id)
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(ENV.SessinCart) != null && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(ENV.SessinCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(ENV.SessinCart);
+            }
+
+            shoppingCartList.Add(new ShoppingCart { ProductId = id });
+            HttpContext.Session.Set(ENV.SessinCart, shoppingCartList);
+            return RedirectToAction(nameof(Index));
+
         }
 
         public IActionResult Privacy()
--- Shop/ENV.cs
+++ Shop/ENV.cs
@@ -8,5 +8,6 @@ namespace Shop
     public class ENV
     {
         public const string ImagePath = @"\images\products\";
+        public const string SessinCart = "ShoppingCartSession";
     }
 }
