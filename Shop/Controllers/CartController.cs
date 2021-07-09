edfca7a2fb1a4f97eae5808c80052310359f391d
trofimchuk <trofimchuk@itstep.academy>
2021-07-09 11:23:05 +0300

    Start woth order

--- Shop/Controllers/CartController.cs
+++ Shop/Controllers/CartController.cs
@@ -1,18 +1,26 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Data;
 using Shop.Models;
+using Shop.Models.ViewModels;
 using Shop.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Shop.Controllers
 {
+    [Authorize]
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public ProductUserVM ProductUserVM { get; set; }
+
         public CartController(ApplicationDbContext db)
         {
             _db = db;
@@ -44,6 +52,31 @@ namespace Shop.Controllers
             shoppingCartList.Remove(shoppingCartList.FirstOrDefault(u => u.ProductId == id));
             HttpContext.Session.Set(ENV.SessinCart, shoppingCartList);
             return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Order()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(ENV.SessinCart) != null && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(ENV.SessinCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(ENV.SessinCart);
+            }
+
+            List<int> productListInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> productList = _db.Product.Where(i => productListInCart.Contains(i.Id));
+
+            ProductUserVM = new ProductUserVM()
+            {
+                AppUser = _db.AppUser.FirstOrDefault(u => u.Id == claim.Value),
+                ProductList = productList.ToList()
+            };
+
+
+            return View(ProductUserVM);
         }
     }
 }
---
+++ Shop/Models/ViewModels/ProductUserVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Models.ViewModels
+{
+    public class ProductUserVM
+    {
+        public ProductUserVM()
+        {
+            ProductList = new List<Product>();
+        }
+
+        public AppUser AppUser { get; set; }
+        public IList<Product> ProductList { get; set; }
+    }
+}
